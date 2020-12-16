ed14a053322fe471f0cbf907a2ed71c021f171a2
KlickKeyn <denak-47-98@mail.ru>
2020-12-16 17:57:48 +0300

    1. Main clear 2. Create constructor and random method in Shop

--- src/main/java/com/klickkeyn/shop/Main.java
+++ src/main/java/com/klickkeyn/shop/Main.java
@@ -1,38 +1,7 @@
 package com.klickkeyn.shop;
 
-import com.klickkeyn.shop.people.Buyer;
-import com.klickkeyn.shop.product.Product;
-import com.klickkeyn.shop.productStorage.productStorage;
-
 public class Main {
     public static void main(String[] args) {
-        Product product1 = new Product("Презики", 400, 100);
-        Product product2 = new Product("Мясо", 800, 100);
-
-        productStorage stall1 = new productStorage("Прилавок с презиками");
-        productStorage stall2 = new productStorage("Прилавок с мясом");
-        stall1.pushProduct(product1);
-        stall2.pushProduct(product2);
-
-        productStorage basket = new productStorage("Корзина");
-
-        Buyer buyer1 = new Buyer(2000);
-        buyer1.takeBasket(basket);
-
-        product1 = stall1.popProduct(1);
-        buyer1.pushProduct(product1);
-        System.out.println(basket.getProdCnt());
-        System.out.println(basket.getPrice());
-
-        product2 = stall2.popProduct(2);
-        buyer1.pushProduct(product2);
-        System.out.println(basket.getProdCnt());
-        System.out.println(basket.getPrice());
-
-        buyer1.popProduct();
-        System.out.println(basket.getProdCnt());
-        System.out.println(basket.getPrice());
-
         System.out.println();
     }
 }
--- src/main/java/com/klickkeyn/shop/store/Shop.java
+++ src/main/java/com/klickkeyn/shop/store/Shop.java
@@ -1,4 +1,50 @@
 package com.klickkeyn.shop.store;
 
+import com.klickkeyn.shop.cashbox.Cashbox;
+import com.klickkeyn.shop.people.Buyer;
+import com.klickkeyn.shop.people.Seller;
+import com.klickkeyn.shop.product.Product;
+import com.klickkeyn.shop.productStorage.productStorage;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Shop {
+    public Shop() {
+        // Stalls are filled with products
+        Product fish = new Product("Рыба", 150, 70);
+        productStorage stallFish = new productStorage("Рыбный прилавок");
+
+        Product meat = new Product("Мясо", 200, 50);
+        productStorage stallMeat = new productStorage("Мясной прилавок");
+
+        Product mayonnaise = new Product("Майонез", 50, 60);
+        productStorage stallMayonnaise = new productStorage("Прилавок с майонезом");
+
+        Product condoms = new Product("Майонез", 280, 40);
+        productStorage stallCondoms = new productStorage("Лоток с презервативами");
+
+        // Create cashbox
+        Cashbox cashbox = new Cashbox(0);
+
+        // The seller come to work
+        Seller seller = new Seller("Галечка");
+
+        //The buyers come to shop
+        Queue<Buyer> queue = new LinkedList<Buyer>();
+        HashSet<Buyer> set = new HashSet<Buyer>();
+        Iterator<Buyer> setIter = set.iterator();
+    }
+
+    private int randomVal(int min, int max) {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
+
+    public void simulateShopWork() {
+
+    }
 }
+
